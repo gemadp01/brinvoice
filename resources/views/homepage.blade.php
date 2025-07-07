ddcc918278a3b73204e6b3711ddf2b3286b0ef37
Gema Dodi Pranata <gemadp01@gmail.com>
2025-07-07 19:41:55 +0700

    fix(nav in the homepage): if the user hasn't logged in yet

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,7 +22,12 @@
             <div class="w-full flex justify-between items-center ms-6">
                 <h1 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">Brinvoice.</h1>
 
+                <div class="hidden sm:block sm:space-x-3 sm:mr-4">
+                    <x-secondary-button>Login</x-secondary-button>
+                    <x-primary-button>Daftar</x-primary-button>
+                </div>
                 <!-- Settings Dropdown -->
+                @if(auth()->user())
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
@@ -41,7 +46,7 @@
                             <x-dropdown-link :href="route('dashboard')">
                                 {{ __('Dashboard') }}
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('profile.edit')" class="border-t border-gray-200 dark:border-gray-600">
+                                <x-dropdown-link :href="route('profile.edit')" class="border-t border-gray-200 dark:border-gray-600">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
@@ -58,9 +63,10 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
+                @endif
 
                 <!-- Hamburger -->
-                <div class="-me-2 flex items-center sm:hidden relative">
+                <div class="flex items-center sm:hidden relative">
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -70,6 +76,15 @@
                 </div>
                 <!-- Responsive Navigation Menu -->
                 <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden absolute right-4 top-[68px] bg-gray-700 rounded-md ">
+                    <div class="pt-2 pb-3 space-y-1">
+                        <x-responsive-nav-link :href="route('login')">
+                            {{ __('Login') }}
+                        </x-responsive-nav-link>
+                        <x-responsive-nav-link :href="route('register')">
+                            {{ __('Daftar') }}
+                        </x-responsive-nav-link>
+                    </div>
+                    @if(auth()->user())
                     <div class="pt-2 pb-3 space-y-1">
                         <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
@@ -100,6 +115,7 @@
                             </form>
                         </div>
                     </div>
+                    @endif
                 </div>
             </div>
 
